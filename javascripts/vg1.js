3032ef96fc5a6fbeaa1d184af267c8058a3567e0
Michał Mikołajczyk <michal@trewebs.com>
2016-11-09 10:15:02 +0100

    Viking Garage, RC2

--- javascripts/vg1.js
+++ javascripts/vg1.js
@@ -1,5 +1,24 @@
 (function ($) {
   $(document).ready(function() {
-      ytbg("Yn1ETOCSRKA");
+      ytbg('rp8CDftiIgs');
+      setTimeout(animateRaido, 6765);
   });
 }(jQuery));
+
+
+function animateRaido() {
+    const outerR = $('.r-replace');
+    const innerR = $('.r-replace span');
+    const callToAction = $('.call-to-action a');
+    innerR.fadeOut(987, function () {
+      const newHTML = '<span class="junicode raido" style="display: none;">&#5809;</span>';
+      outerR.html(newHTML);
+      const raido = $('.raido');
+      setTimeout(function() {
+        raido.fadeIn(987, setTimeout.bind(this, function () {
+          raido.addClass('runic-red')
+          setTimeout(callToAction.addClass.bind(callToAction, 'blink'), 4181)
+        }, 1597));
+      }, 987);
+    });
+}
