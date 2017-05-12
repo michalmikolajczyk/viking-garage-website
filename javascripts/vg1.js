3539ab9ac863c18f27315e3771940e964a625f76
Michal Mikolajczyk <michal@trewebs.com>
2017-05-12 04:08:47 +0200

    wersja polska

--- javascripts/vg1.js
+++ javascripts/vg1.js
@@ -1,24 +1,25 @@
 (function ($) {
   $(document).ready(function() {
-      ytbg('rp8CDftiIgs');
-      setTimeout(animateRaido, 6765);
+      // ytbg('rp8CDftiIgs');
+      setTimeout(animateRaido, 1597);
   });
 }(jQuery));
 
 
 function animateRaido() {
-    const outerR = $('.r-replace');
-    const innerR = $('.r-replace span');
+    // const outerR = $('.r-replace');
+    // const innerR = $('.r-replace span');
     const callToAction = $('.call-to-action a');
-    innerR.fadeOut(987, function () {
-      const newHTML = '<span class="junicode raido" style="display: none;">&#5809;</span>';
-      outerR.html(newHTML);
-      const raido = $('.raido');
-      setTimeout(function() {
-        raido.fadeIn(987, setTimeout.bind(this, function () {
-          raido.addClass('runic-red')
-          setTimeout(callToAction.addClass.bind(callToAction, 'blink'), 4181)
-        }, 1597));
-      }, 987);
-    });
+    // innerR.fadeOut(987, function () {
+    //   const newHTML = '<span class="junicode raido" style="display: none;">&#5809;</span>';
+    //   outerR.html(newHTML);
+    const raido = $('.raido');
+    raido.addClass('runic-red')
+    // setTimeout(function() {
+      // raido.fadeIn(987, setTimeout.bind(this, function () {
+        // raido.addClass('runic-red')
+        // setTimeout(callToAction.addClass.bind(callToAction, 'blink'), 4181)
+      // }, 1597));
+    // }, 987);
+    // });
 }
